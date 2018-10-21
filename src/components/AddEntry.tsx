f42fa0d3ccbc9e9ec6dad4a3059f482f3692ef90
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-10-21 19:12:46 +0300

    Use new SelectItem

--- src/components/AddEntry.tsx
+++ src/components/AddEntry.tsx
@@ -1,16 +1,14 @@
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
-import Input from "@material-ui/core/Input";
+import {notEmpty} from "@epeli/utils";
+import {values} from "lodash-es";
 import React from "react";
 import {MappedActions} from "redux-render-prop";
 
 import {createMyHoursConnect} from "../redux/create-connect";
-import {DayID, Project} from "../redux/state";
+import {DayID} from "../redux/state";
 import {generateEntryId} from "../redux/state-tools";
 
-import {Row, View} from "./core";
-import SelectProject from "./SelectProject";
-import SelectProject2 from "./SelectProject2";
+import {View} from "./core";
+import SelectItem from "./SelectItem";
 
 const EntryConnect = createMyHoursConnect({
     mapActions: actions => ({
@@ -18,64 +16,41 @@ const EntryConnect = createMyHoursConnect({
     }),
 });
 
-const initialState = {
-    comment: "",
-    project: null as Project | null,
-    duration: 0,
-};
+const ProjectsConnect = createMyHoursConnect({
+    mapState: selectors => selectors.state.projects,
+});
 
-class AddEntry extends React.Component<{day: DayID}, typeof initialState> {
-    state = initialState;
-
+class AddEntry extends React.Component<{day: DayID}> {
     render() {
         return (
             <EntryConnect render={(...args) => this.renderInputs(...args)} />
         );
     }
 
-    canAdd() {
-        return Boolean(this.state.project);
-    }
-
     renderInputs(_: unknown, actions: MappedActions<typeof EntryConnect>) {
         return (
             <View>
-                <SelectProject2 />
-                <SelectProject
-                    project={this.state.project}
-                    onChange={project => {
-                        this.setState({project});
-                    }}
+                <ProjectsConnect
+                    render={projects => (
+                        <SelectItem
+                            items={values(projects)
+                                .filter(notEmpty)
+                                .map(p => ({label: p.name, id: p.id}))}
+                            onInputValueChange={() => {}}
+                            onSelect={project => {
+                                actions.addEntry({
+                                    day: this.props.day,
+                                    entry: {
+                                        id: generateEntryId(),
+                                        projectID: project.id,
+                                        start: Date.now(),
+                                        comment: "",
+                                    },
+                                });
+                            }}
+                        />
+                    )}
                 />
-                <Input
-                    placeholder="Comment"
-                    value={this.state.comment}
-                    onChange={e => {
-                        this.setState({comment: e.target.value});
-                    }}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    disabled={!this.canAdd()}
-                    onClick={() => {
-                        if (!this.state.project) {
-                            return;
-                        }
-                        actions.addEntry({
-                            day: this.props.day,
-                            entry: {
-                                id: generateEntryId(),
-                                projectID: this.state.project.id,
-                                comment: this.state.comment,
-                                start: Date.now(),
-                            },
-                        });
-                    }}
-                >
-                    Send
-                    <Icon>send</Icon>
-                </Button>
             </View>
         );
     }
--- src/components/SelectItem.tsx
+++ src/components/SelectItem.tsx
@@ -1,46 +1,9 @@
 import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
-import Downshift from "downshift";
+import Downshift, {DownshiftInterface} from "downshift";
 import matchSorter from "match-sorter";
 import React from "react";
-
-const suggestions = [
-    {label: "Afghanistan"},
-    {label: "Aland Islands"},
-    {label: "Albania"},
-    {label: "Algeria"},
-    {label: "American Samoa"},
-    {label: "Andorra"},
-    {label: "Angola"},
-    {label: "Anguilla"},
-    {label: "Antarctica"},
-    {label: "Antigua and Barbuda"},
-    {label: "Argentina"},
-    {label: "Armenia"},
-    {label: "Aruba"},
-    {label: "Australia"},
-    {label: "Austria"},
-    {label: "Azerbaijan"},
-    {label: "Bahamas"},
-    {label: "Bahrain"},
-    {label: "Bangladesh"},
-    {label: "Barbados"},
-    {label: "Belarus"},
-    {label: "Belgium"},
-    {label: "Belize"},
-    {label: "Benin"},
-    {label: "Bermuda"},
-    {label: "Bhutan"},
-    {label: "Bolivia, Plurinational State of"},
-    {label: "Bonaire, Sint Eustatius and Saba"},
-    {label: "Bosnia and Herzegovina"},
-    {label: "Botswana"},
-    {label: "Bouvet Island"},
-    {label: "Brazil"},
-    {label: "British Indian Ocean Territory"},
-    {label: "Brunei Darussalam"},
-];
 
 function renderInput(inputProps: any) {
     const {InputProps, ref, ...other} = inputProps;
@@ -87,16 +50,33 @@ function renderSuggestion({
     );
 }
 
-function getSuggestions(value: string) {
+function getSuggestions(suggestions: Item[], value: string) {
     return matchSorter(suggestions, value, {keys: ["label"]}).slice(0, 10);
 }
 
-function IntegrationDownshift() {
+interface Item {
+    label: string;
+}
+
+interface Props<T extends Item> {
+    items: T[];
+    onSelect(item: T): void;
+    onInputValueChange(value: string): void;
+}
+
+const TypedDownshift: DownshiftInterface<Item> = Downshift;
+
+function SelectItem<T extends Item>(props: Props<T>) {
     return (
         <div>
-            <Downshift
-                onSelect={e => {
-                    console.log("select", e);
+            <TypedDownshift
+                onSelect={item => {
+                    props.onSelect(item as T);
+                    console.log("select", item);
+                }}
+                itemToString={item => (item ? item.label : "")}
+                onInputValueChange={value => {
+                    props.onInputValueChange(value);
                 }}
                 onStateChange={(changes, state) => {
                     if (!state.highlightedIndex) {
@@ -115,27 +95,29 @@ function IntegrationDownshift() {
                         <div {...ds.getMenuProps()}>
                             {ds.isOpen ? (
                                 <Paper square>
-                                    {getSuggestions(ds.inputValue || "").map(
-                                        (suggestion, index) =>
-                                            renderSuggestion({
-                                                suggestion,
-                                                index,
-                                                itemProps: ds.getItemProps({
-                                                    item: suggestion.label,
-                                                }),
-                                                highlightedIndex:
-                                                    ds.highlightedIndex,
-                                                selectedItem: ds.selectedItem,
+                                    {getSuggestions(
+                                        props.items,
+                                        ds.inputValue || "",
+                                    ).map((suggestion, index) =>
+                                        renderSuggestion({
+                                            suggestion,
+                                            index,
+                                            itemProps: ds.getItemProps({
+                                                item: suggestion,
                                             }),
+                                            highlightedIndex:
+                                                ds.highlightedIndex,
+                                            selectedItem: ds.selectedItem,
+                                        }),
                                     )}
                                 </Paper>
                             ) : null}
                         </div>
                     </div>
                 )}
-            </Downshift>
+            </TypedDownshift>
         </div>
     );
 }
 
-export default IntegrationDownshift;
+export default SelectItem;
