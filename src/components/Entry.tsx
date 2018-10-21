777404ef4ad8f40b0afe09b352428bf0bd5dfa53
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-10-21 15:00:55 +0300

    cleanup

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,6 +1,4 @@
 import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/lab/Slider";
