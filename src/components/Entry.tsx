61949d4d5a7d581bd3a3c471892a5ca4aabe7097
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-10-21 13:47:01 +0300

    Basic slider implementation

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -7,8 +7,10 @@ import * as datefns from "date-fns";
 import {rem} from "polished";
 import React from "react";
 import styled from "react-emotion";
+import {MappedActions} from "redux-render-prop";
 
-import {DayID, EntryID, ProjectID} from "../redux/state";
+import {SimpleActions} from "../redux/actions";
+import {DayID, Entry, EntryID, ProjectID} from "../redux/state";
 import {createMyHoursConnect} from "../redux/store";
 
 import {View} from "./core";
@@ -16,6 +18,9 @@ import {View} from "./core";
 const EntryConnect = createMyHoursConnect({
     mapState: (selectors, props: {day: DayID; id: EntryID}) =>
         selectors.getEntry(props.day, props.id) || null,
+    mapActions: actions => ({
+        setEntryValues: actions.setEntryValues,
+    }),
 });
 
 const ProjectConnect = createMyHoursConnect({
@@ -29,48 +34,112 @@ const Container = styled(Paper)({
 });
 
 const SliderContainer = styled(View)({
+    flexDirection: "row",
     padding: rem(16),
     margin: rem(16),
 });
 
-const Entry = (props: {day: DayID; id: EntryID}) => (
+interface Props {
+    day: DayID;
+    entry: Entry;
+    setEntryValues: MappedActions<typeof EntryConnect>["setEntryValues"];
+}
+
+interface State {
+    sliderValue: number | null;
+    // setEntryValues: typeof SimpleActions["setEntryValues"];
+}
+
+class EntryClass extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {sliderValue: null};
+    }
+
+    handleSlider = (e: unknown, value: number) => {
+        this.setState({sliderValue: value});
+    };
+
+    handleSliderDragEnd = () => {
+        if (typeof this.state.sliderValue !== "number") {
+            return;
+        }
+
+        this.props.setEntryValues({
+            day: this.props.day,
+            entryID: this.props.entry.id,
+            values: {
+                end:
+                    this.props.entry.start + this.state.sliderValue * 60 * 1000,
+            },
+        });
+
+        this.setState({sliderValue: null});
+    };
+
+    getSliderValue(): number {
+        if (typeof this.state.sliderValue === "number") {
+            return this.state.sliderValue;
+        }
+
+        const start = this.props.entry.start;
+        const end = this.props.entry.end || Date.now();
+
+        return (end - start) / 1000 / 60;
+    }
+
+    render() {
+        const {entry} = this.props;
+
+        return (
+            <Container>
+                <Typography variant="h5" component="h2">
+                    <ProjectConnect
+                        id={entry.projectID}
+                        render={data => (
+                            <span>
+                                {data.name}{" "}
+                                {datefns.format(entry.start, "HH:mm")}
+                                {" - "}
+                                {datefns.format(
+                                    entry.end || Date.now(),
+                                    "HH:mm",
+                                )}{" "}
+                                {!entry.end ? "and counting..." : ""}
+                            </span>
+                        )}
+                    />
+                </Typography>
+
+                <SliderContainer>
+                    {this.getSliderValue()} min
+                    <Slider
+                        min={0}
+                        max={60 * 5}
+                        step={1}
+                        value={this.getSliderValue()}
+                        onChange={this.handleSlider}
+                        onDragEnd={this.handleSliderDragEnd}
+                    />
+                </SliderContainer>
+
+                <Typography component="p">{entry.comment}</Typography>
+            </Container>
+        );
+    }
+}
+
+const EntryWrap = (props: {day: DayID; id: EntryID}) => (
     <EntryConnect
         day={props.day}
         id={props.id}
-        render={entry =>
+        render={(entry, actions) =>
             entry ? (
-                <Container>
-                    <Typography variant="h5" component="h2">
-                        <ProjectConnect
-                            id={entry.projectID}
-                            render={data => (
-                                <span>
-                                    {data.name}{" "}
-                                    {datefns.format(entry.start, "HH:mm")}
-                                    {" - "}
-                                    {datefns.format(
-                                        entry.end || Date.now(),
-                                        "HH:mm",
-                                    )}
-                                </span>
-                            )}
-                        />
-                    </Typography>
-
-                    <SliderContainer>
-                        <Slider
-                            min={0}
-                            max={60 * 5}
-                            step={1}
-                            value={60}
-                            onChange={(e, value) => {
-                                console.log("val", value);
-                            }}
-                        />
-                    </SliderContainer>
-
-                    <Typography component="p">{entry.comment}</Typography>
-                </Container>
+                <EntryClass
+                    day={props.day}
+                    entry={entry}
+                    setEntryValues={actions.setEntryValues}
+                />
             ) : (
                 "not found: " + props.id
             )
@@ -78,4 +147,4 @@ const Entry = (props: {day: DayID; id: EntryID}) => (
     />
 );
 
-export default Entry;
+export default EntryWrap;
--- src/redux/actions.tsx
+++ src/redux/actions.tsx
@@ -1,6 +1,7 @@
 import {createSimpleActions} from "@epeli/redux-stack";
+import {Omit, strictAssign} from "@epeli/utils";
 
-import {DayID, Entry, initialState} from "./state";
+import {DayID, Entry, EntryID, initialState} from "./state";
 
 export const SimpleActions = createSimpleActions(initialState, {
     setCount(draftState, action: {newCount: number}) {
@@ -16,6 +17,32 @@ export const SimpleActions = createSimpleActions(initialState, {
         }
 
         day.entries.push(action.entry);
+
+        return draftState;
+    },
+
+    setEntryValues(
+        draftState,
+        action: {
+            day: DayID;
+            entryID: EntryID;
+            values: Partial<Omit<Entry, "id">>;
+        },
+    ) {
+        const day = draftState.days[action.day];
+
+        if (!day) {
+            console.warn("Unknown day: " + action.day);
+            return draftState;
+        }
+
+        const entry = day.entries.find(_entry => _entry.id === action.entryID);
+
+        if (!entry) {
+            return draftState;
+        }
+
+        strictAssign(entry, action.values);
 
         return draftState;
     },
